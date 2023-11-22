e408c1872d8da6e4a4179416f0ae78cf15b9b846
장청룡 <wkdcjdfyd@naver.com>
2023-11-22 09:38:40 +0900

    Fix: place에 지번 주소 추가

--- src/main/java/com/jejujaju/place/model/dto/PlaceRequestDto.java
+++ src/main/java/com/jejujaju/place/model/dto/PlaceRequestDto.java
@@ -14,6 +14,7 @@ public class PlaceRequestDto {
     private String name;
     private String description;
     private String addr;
+    private String addressName;
     private String phone;
     private String url;
     private String img;
